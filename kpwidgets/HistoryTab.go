fba544aabcc1d0d7625b8ea5e7b488119996f195
yangwenlong <tecyang@gmail.com>
2023-07-25 21:41:38 +0800

    add HistoryTab widget

--- kpwidgets/HistoryTab.go
+++ kpwidgets/HistoryTab.go
@@ -4,78 +4,41 @@ import (
 	"fmt"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
-	"os/exec"
 )
 
-type AutoTypeTab struct {
-	Widget                            *widgets.QWidget
-	EnableAutoTypeCheckbox            *widgets.QCheckBox
-	InheritDefaultTypeRadio           *widgets.QRadioButton
-	OverrideDefaultSequenceRadio      *widgets.QRadioButton
-	OverrideDefaultSequenceInput      *widgets.QLineEdit
-	OverrideDefaultSequenceEditButton *widgets.QPushButton
-	WindowSequenceTable               *widgets.QTableView
-	TwoChannelAutoTypeCheckbox        *widgets.QCheckBox
-	TableWidget                       *widgets.QTableWidget
+type HistoryTab struct {
+	Widget        *widgets.QWidget
+	TableWidget   *widgets.QTableWidget
+	ViewButton    *widgets.QPushButton
+	DeleteButton  *widgets.QPushButton
+	RestoreButton *widgets.QPushButton
 }
 
-func NewAutoTypeTab() *AutoTypeTab {
-	cw := &AutoTypeTab{
-		Widget:                       widgets.NewQWidget(nil, 0),
-		EnableAutoTypeCheckbox:       widgets.NewQCheckBox2("Enable auto-type for this entry", nil),
-		InheritDefaultTypeRadio:      widgets.NewQRadioButton2("Inherit default auto-type sequence from group", nil),
-		OverrideDefaultSequenceRadio: widgets.NewQRadioButton2("Override default sequence", nil),
-		OverrideDefaultSequenceInput: widgets.NewQLineEdit(nil),
-		TableWidget:                  widgets.NewQTableWidget(nil),
-
-		OverrideDefaultSequenceEditButton: widgets.NewQPushButton2("Edit", nil),
-		TwoChannelAutoTypeCheckbox:        widgets.NewQCheckBox2("Two-channel auto-type obfuscation", nil),
+func NewHistoryTab() *HistoryTab {
+	cw := &HistoryTab{
+		Widget:        widgets.NewQWidget(nil, 0),
+		TableWidget:   widgets.NewQTableWidget(nil),
+		ViewButton:    widgets.NewQPushButton2("View", nil),
+		DeleteButton:  widgets.NewQPushButton2("Delete", nil),
+		RestoreButton: widgets.NewQPushButton2("Restore", nil),
 	}
 
 	// Layout
 	vBoxLayout := widgets.NewQVBoxLayout2(cw.Widget)
 
-	vBoxLayout.AddWidget(cw.EnableAutoTypeCheckbox, 0, core.Qt__AlignLeft)
-	vBoxLayout.AddWidget(cw.InheritDefaultTypeRadio, 0, core.Qt__AlignLeft)
-	vBoxLayout.AddWidget(cw.OverrideDefaultSequenceRadio, 0, core.Qt__AlignLeft)
-
-	overrideDefaultSequenceLayout := widgets.NewQHBoxLayout2(nil)
-	overrideDefaultSequenceLayout.AddSpacing(40)
-	cw.OverrideDefaultSequenceInput.SetFixedWidth(400)
-	overrideDefaultSequenceLayout.AddWidget(cw.OverrideDefaultSequenceInput, 0, core.Qt__AlignLeft)
-	overrideDefaultSequenceLayout.AddWidget(cw.OverrideDefaultSequenceEditButton, 1, core.Qt__AlignLeft)
-	vBoxLayout.AddLayout(overrideDefaultSequenceLayout, 0)
-
-	setTableWidget(cw.TableWidget, vBoxLayout)
+	setHistoryTableWidget(cw.TableWidget, vBoxLayout)
 	/*vBoxLayout.AddWidget(cw.TagsLabel, 0, core.Qt__AlignLeft)
 	vBoxLayout.AddWidget(cw.TagsInput, 0, core.Qt__AlignLeft)*/
 
 	twoChannelAutoTypeLayout := widgets.NewQHBoxLayout2(nil)
-	twoChannelAutoTypeLabel := widgets.NewQLabel2("link", nil, 0)
-	twoChannelAutoTypeLabel.SetText("<a href=\"D:\\Program Files\\TotalCMDPortable\\App\\totalcmd\\Plugins\\wcx\\Total7zip\\7-zip.chm\">What is this?</a>")
+	twoChannelAutoTypeLayout.AddWidget(cw.ViewButton, 0, core.Qt__AlignLeft)
+	twoChannelAutoTypeLayout.AddWidget(cw.DeleteButton, 0, core.Qt__AlignTop)
 
-	twoChannelAutoTypeLabel.SetTextInteractionFlags(core.Qt__LinksAccessibleByMouse)
-
-	twoChannelAutoTypeLabel.ConnectLinkActivated(func(link string) {
-		//link = "D:\\Program Files\\TotalCMDPortable\\App\\totalcmd\\Plugins\\wcx\\Total7zip\\7-zip.chm"
-		if err := exec.Command("hh.exe", link).Start(); err != nil {
-			fmt.Println("Error opening .chm file:", err)
-		}
-	})
-	twoChannelAutoTypeLayout.AddWidget(cw.TwoChannelAutoTypeCheckbox, 0, core.Qt__AlignLeft)
-	twoChannelAutoTypeLayout.AddWidget(twoChannelAutoTypeLabel, 0, core.Qt__AlignTop)
+	spacer := widgets.NewQSpacerItem(40, 20, widgets.QSizePolicy__Expanding, widgets.QSizePolicy__Minimum)
+	twoChannelAutoTypeLayout.AddSpacerItem(spacer)
+	twoChannelAutoTypeLayout.AddWidget(cw.ViewButton, 0, core.Qt__AlignLeft)
+	twoChannelAutoTypeLayout.AddWidget(cw.RestoreButton, 0, core.Qt__AlignTop)
 	vBoxLayout.AddLayout(twoChannelAutoTypeLayout, 0)
-
-	// Connect the "Pick Color" buttons clicked signal to open the color selection dialogs
-	cw.InheritDefaultTypeRadio.ConnectClicked(func(checked bool) {
-		cw.OverrideDefaultSequenceInput.SetDisabled(checked)
-		cw.OverrideDefaultSequenceEditButton.SetDisabled(checked)
-	})
-
-	cw.OverrideDefaultSequenceRadio.ConnectClicked(func(checked bool) {
-		cw.OverrideDefaultSequenceInput.SetDisabled(!checked)
-		cw.OverrideDefaultSequenceEditButton.SetDisabled(!checked)
-	})
 
 	/*	cw.BackgroundColorPickerBtn.ConnectClicked(func(checked bool) {
 		colorDialog := widgets.NewQColorDialog2(gui.NewQColor3(255, 255, 255, 255), nil)
@@ -88,27 +51,17 @@ func NewAutoTypeTab() *AutoTypeTab {
 	return cw
 }
 
-func setTableWidget(tableWidget *widgets.QTableWidget, vBoxLayout *widgets.QVBoxLayout) {
-	tableLable := widgets.NewQLabel2("Use custom sequences for specific window:", nil, 0)
-	vBoxLayout.AddWidget(tableLable, 0, core.Qt__AlignLeft)
+func setHistoryTableWidget(tableWidget *widgets.QTableWidget, vBoxLayout *widgets.QVBoxLayout) {
 
-	tableWidget.SetColumnCount(2)
+	tableWidget.SetColumnCount(4)
 	tableWidget.SetFixedWidth(600)
-	tableWidget.SetColumnWidth(0, 300)
-	tableWidget.SetColumnWidth(0, 300)
 	// Set the header labels
-	headerLabels := []string{"Taget Window", "Sequence"}
+	headerLabels := []string{"Version", "Title", "User Name", "Size"}
 	tableWidget.SetHorizontalHeaderLabels(headerLabels)
-
-	tableButtonVLayout := getTableButtonVLayout(tableWidget)
-
-	tableHLayout := widgets.NewQHBoxLayout2(nil)
-	tableHLayout.AddWidget(tableWidget, 0, core.Qt__AlignLeft)
-	tableHLayout.AddLayout(tableButtonVLayout, 0)
-	vBoxLayout.AddLayout(tableHLayout, 0)
+	vBoxLayout.AddWidget(tableWidget, 0, core.Qt__AlignLeft)
 }
 
-func getTableButtonVLayout(tableWidget *widgets.QTableWidget) *widgets.QVBoxLayout {
+func getHistoryTableButtonVLayout(tableWidget *widgets.QTableWidget) *widgets.QVBoxLayout {
 	tableButtonVLayout := widgets.NewQVBoxLayout2(nil)
 	addButton := widgets.NewQPushButton2("Add", nil)
 	editButton := widgets.NewQPushButton2("Edit", nil)
