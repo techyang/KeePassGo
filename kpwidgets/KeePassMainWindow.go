c803f79b559a46a65c708e0eaa4f7b253a172460
yangwenlong <tecyang@gmail.com>
2023-07-30 11:40:32 +0800

    Refactoring code

--- kpwidgets/KeePassMainWindow.go
+++ kpwidgets/KeePassMainWindow.go
@@ -1,7 +1,6 @@
 package kpwidgets
 
 import (
-	"fmt"
 	"github.com/techyang/keepassgo/functions"
 	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
@@ -33,36 +32,12 @@ func InitMainWindow() *widgets.QMainWindow {
 	window.Resize2(800, 650)
 
 	sys := InitTrayIcon(window)
-	menu := getTrayContextMenu()
-
+	//menu := getTrayContextMenu()
+	trayContextMenu := NewTrayContextMenu()
 	//设置菜单
-	sys.SetContextMenu(menu)
+	sys.SetContextMenu(trayContextMenu)
 	sys.Show()
 	return window
-}
-
-func getTrayContextMenu() *widgets.QMenu {
-	menu := widgets.NewQMenu(nil)
-	exit := menu.AddAction("Exit")
-	exit.ConnectTriggered(func(bool) {
-		//app.Exit(0)
-	})
-	//添加分隔符
-	menu.AddSeparator()
-	help := menu.AddAction("help")
-	//定义二级菜单
-	menuChild := widgets.NewQMenu(nil)
-	menuChild.AddAction("option")
-	about := menuChild.AddAction("about")
-	about.ConnectTriggered(func(bool) {
-		//button := widgets.QMessageBox_Information(nil, "title", "text", widgets.QMessageBox__Ok, widgets.QMessageBox__Yes)
-		fmt.Println("click me")
-		//widgets.QMessageBox_Information(nil, "title", "text", widgets.QMessageBox__Ok, widgets.QMessageBox__Yes)
-		widgets.NewQFileDialog2(nil, "打开", "d:", "*.txt").Show()
-	})
-	//设置子项
-	help.SetMenu(menuChild)
-	return menu
 }
 
 func InitTrayIcon(window *widgets.QMainWindow) *widgets.QSystemTrayIcon {
--- kpwidgets/MainContent.go
+++ kpwidgets/MainContent.go
@@ -1,12 +1,9 @@
 package kpwidgets
 
 import (
-	"fmt"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
-	"github.com/tobischo/gokeepasslib/v3"
-	"os"
 )
 
 type PasswordDelegate struct {
@@ -48,9 +45,8 @@ func InitMainContent(window *widgets.QMainWindow) {
 	//button2 := widgets.NewQPushButton2("Button 2", nil)
 
 	// Create a QTreeWidget
-	//tableWidget := initTableWidget()
 	tableWidget := NewKeePassTable()
-	treeWidget := initTreeWidget(tableWidget)
+	treeWidget := NewKeePassTree(tableWidget)
 
 	//restTable(tableWidget)
 
@@ -68,102 +64,6 @@ func InitMainContent(window *widgets.QMainWindow) {
 	// Set the container as the central widget of the main window
 	window.SetCentralWidget(splitter)
 
-}
-
-func initTreeWidget(tableWidget *KeePassTable) *widgets.QTreeWidget {
-	treeWidget := widgets.NewQTreeWidget(nil)
-	//treeWidget.SetHeaderLabels([]string{"yangwl"})
-	file, _ := os.Open("D:\\workspace_go\\gokeepasslib-master\\example-new-database2023.kdbx")
-
-	db := gokeepasslib.NewDatabase()
-	db.Credentials = gokeepasslib.NewPasswordCredentials("111111")
-	_ = gokeepasslib.NewDecoder(file).Decode(db)
-
-	db.UnlockProtectedEntries()
-	rootGroups := db.Content.Root.Groups
-	for i, rootGroup := range rootGroups {
-		fmt.Println(i, "rootGroup:", rootGroup.Name)
-		// Create the root item
-		rootItem := widgets.NewQTreeWidgetItem4(treeWidget, []string{rootGroup.Name, "1.1"}, 0)
-		rootItem.SetExpanded(true) // Set the root item initially expanded
-		groups := rootGroup.Groups
-		buildGroupTree(rootItem, groups)
-
-		treeWidget.InsertTopLevelItem(i, rootItem)
-	}
-	// Create the root item
-
-	// Set the root item as the top-level item of the tree widget
-
-	treeWidget.SetHeaderHidden(true)
-
-	// Connect the itemClicked signal of the tree widget
-	TreeItemClicked(tableWidget, treeWidget, rootGroups)
-	return treeWidget
-}
-
-func buildGroupTree(parent *widgets.QTreeWidgetItem, groups []gokeepasslib.Group) {
-	for _, group := range groups {
-		txt, _ := group.UUID.MarshalText()
-
-		fmt.Println("group.UUID -----------:", group.Name, ":", string(txt))
-		treeItem := widgets.NewQTreeWidgetItem2([]string{group.Name}, 0)
-		//treeItem.SetData(0, core.Qt__UserRole, core.NewQVariant1(group.UUID.String()))
-
-		treeItem.SetData(1, 0, core.NewQVariant1(string(txt)))
-
-		parent.AddChild(treeItem)
-		buildGroupTree(treeItem, group.Groups)
-	}
-}
-
-func TreeItemClicked(tableWidget *KeePassTable, treeWidget *widgets.QTreeWidget, rootGroups []gokeepasslib.Group) {
-
-	treeWidget.ConnectItemClicked(func(item *widgets.QTreeWidgetItem, column int) {
-
-		groupUUID := item.Data(1, 0).ToString()
-		fmt.Println(item.Text(0), "点击了", groupUUID)
-		tableWidget.SetObjectName(groupUUID)
-		group := findGroupByUUID(rootGroups, groupUUID)
-
-		if group != nil && group.Entries != nil {
-			//headerLabels := []string{"Title", "User Name", "Password", "URL", "Notes"}
-			//tableWidget.SetHorizontalHeaderLabels(headerLabels)
-			setTableItems(group, tableWidget)
-		} else {
-			//headerLabels := []string{"Title", "User Name", "Password", "URL", "Notes"}
-			//tableWidget.SetHorizontalHeaderLabels(headerLabels)
-			//tableWidget.Clear()
-			tableWidget.SetRowCount(0)
-		}
-
-	})
-
-	passwordDelegate := NewPasswordDelegate()
-	tableWidget.SetItemDelegateForColumn(0, passwordDelegate)
-}
-
-func setTableItems(group *gokeepasslib.Group, tableWidget *KeePassTable) {
-	// Set the password delegate for the second column
-	passwordDelegate := NewPasswordDelegate()
-	tableWidget.SetItemDelegateForColumn(0, passwordDelegate)
-	for i, entry := range group.Entries {
-		username := entry.Get("UserName").Value.Content
-		url := entry.Get("URL").Value.Content
-		note := entry.Get("Notes").Value.Content
-		tableWidget.SetRowCount(i + 1)
-		tableWidget.SetItem(i, 0, widgets.NewQTableWidgetItem2(entry.GetTitle(), 0))
-		tableWidget.SetItem(i, 1, widgets.NewQTableWidgetItem2(username, 0))
-
-		passwordItem := widgets.NewQTableWidgetItem2(entry.GetPassword(), 0)
-		//passwordItem.SetFlags(core.Qt__ItemIsSelectable | core.Qt__ItemIsEditable)
-		//passwordItem.SetFlags(passwordItem.Flags() | core.Qt__ItemIsUserCheckable)
-		//passwordItem.SetCheckState(core.Qt__Checked)
-
-		tableWidget.SetItem(i, 2, passwordItem)
-		tableWidget.SetItem(i, 3, widgets.NewQTableWidgetItem2(url, 0))
-		tableWidget.SetItem(i, 4, widgets.NewQTableWidgetItem2(note, 0))
-	}
 }
 
 func InitToolbar(window *widgets.QMainWindow) {
