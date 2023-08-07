c1493054167ceffaa06c431cba3c415032270c2d
yangwenlong <tecyang@gmail.com>
2023-08-07 23:17:52 +0800

    add toolbar buttons and icons

--- kpwidgets/MainContent.go
+++ kpwidgets/MainContent.go
@@ -72,26 +72,66 @@ func InitToolbar(window *widgets.QMainWindow) {
 	// Add tool buttons to the toolbar
 	newToolButton := widgets.NewQToolButton(nil)
 	newToolButton.SetText("New")
-	newToolIcon := window.Style().StandardIcon(widgets.QStyle__SP_FileIcon, nil, nil)
-	newToolButton.SetIcon(newToolIcon)
+	//newToolIcon := window.Style().StandardIcon(widgets.QStyle__SP_FileIcon, nil, nil)
+	//newToolButton.SetIcon(newToolIcon)
 	//.
-	//newToolButton.SetIcon(gui.NewQIcon5("tr\\docs\\favicon.ico"))
+	newToolButton.SetIcon(gui.NewQIcon5("Resources/Nuvola/B16x16_FileNew.png"))
+	newToolButton.SetToolTip("New")
+	iconSize := newToolButton.IconSize()
+
+	buttonWidth := iconSize.Width() + 10
+	buttonHeight := iconSize.Height() + 8
+	newToolButton.SetFixedSize2(buttonWidth, buttonHeight)
+
+	newToolButton.AdjustSize()
 
 	// Add tool buttons to the toolbar
 	openToolButton := widgets.NewQToolButton(nil)
 	openToolButton.SetToolTip("Open")
+	openToolButton.SetIcon(gui.NewQIcon5("Resources/Nuvola/B16x16_Folder_Yellow_Open.png"))
+	openToolButton.SetFixedSize2(buttonWidth, buttonHeight)
 
-	openToolButton.SetIcon(gui.NewQIcon5("Ext\\Images_Client_16\\C49_Folder_Blue_Open.png"))
+	openToolButton.AdjustSize()
+
+	saveAsToolButton := widgets.NewQToolButton(nil)
+	saveAsToolButton.SetToolTip("Save")
+	saveAsToolButton.SetIcon(gui.NewQIcon5("Resources/Nuvola/B16x16_FileSave.png"))
+	saveAsToolButton.SetFixedSize2(buttonWidth, buttonHeight)
+	saveAsToolButton.AdjustSize()
+
+	addEntityToolButton := widgets.NewQToolButton(nil)
+	addEntityToolButton.SetToolTip("Add Entity")
+	addEntityToolButton.SetIcon(gui.NewQIcon5("Resources/Nuvola/B16x16_KGPG_Import.png"))
+	addEntityToolButton.SetFixedSize2(buttonWidth, buttonHeight)
+	addEntityToolButton.AdjustSize()
+
+	copyUserNameToolButton := widgets.NewQToolButton(nil)
+	copyUserNameToolButton.SetToolTip("Copy UserName to ClipBoard ")
+	copyUserNameToolButton.SetIcon(gui.NewQIcon5("Resources/Nuvola/B16x16_Personal.png"))
+	copyUserNameToolButton.SetFixedSize2(buttonWidth, buttonHeight)
+	copyUserNameToolButton.AdjustSize()
+
+	copyPasswordToolButton := widgets.NewQToolButton(nil)
+	copyPasswordToolButton.SetToolTip("Copy Password to ClipBoard ")
+	//icon := gui.NewQIcon5("Resources/Nuvola/B16x16_KGPG_Info.png")
+	copyPasswordToolButton.SetIcon(gui.NewQIcon5("Resources/Nuvola/B16x16_KGPG_Info.png"))
+	//copyPasswordToolButton.SetSizePolicy2(widgets.QSizePolicy__Fixed, widgets.QSizePolicy__Fixed)
+	copyPasswordToolButton.SetFixedSize2(buttonWidth, buttonHeight)
+	copyPasswordToolButton.AdjustSize()
 
 	/*newAction := widgets.NewQAction3(gui.QIcon_FromTheme("document-new"), "New", nil)
 	openAction := widgets.NewQAction3(gui.QIcon_FromTheme("document-open"), "Open", nil)
 	saveAction := widgets.NewQAction3(gui.QIcon_FromTheme("document-save"), "Save", nil)
-
+	B16x16_FileSaveAs
 	// Add the actions to the toolbar
 	toolBar.AddActions([]*widgets.QAction{newAction, openAction, saveAction})*/
 
 	toolBar.AddWidget(newToolButton)
 	toolBar.AddWidget(openToolButton)
+	toolBar.AddWidget(saveAsToolButton)
+	toolBar.AddWidget(addEntityToolButton)
+	toolBar.AddWidget(copyUserNameToolButton)
+	toolBar.AddWidget(copyPasswordToolButton)
 
 	// Add the toolbar to the main window
 	window.AddToolBar2(toolBar)
